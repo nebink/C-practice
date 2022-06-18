5b08b26fdd004f59f9295344d9ae4f49f7a873e8
Nebin K Raj <96125944+nebink@users.noreply.github.com>
2022-06-18 20:18:50 +0530

    Add files via upload

---
+++  Inserting arr and printing.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no. of values to inserted in the array :");
+    scanf("%d",&a);
+    int (arr[a]);
+    for(int i; i<a; i++){
+        printf("\nenter the values");
+        scanf("%d",&arr[i]);
+    }
+    
+    //to print the array
+    printf("\nThe inserted array is :");
+    
+for (int j ;j<a; j++){
+    printf("\n %d",arr[j]);
+        
+}
+    return 0;
+    
+}
