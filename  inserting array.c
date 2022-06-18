54e386ee3ae6fd55bc6a84210430a80472ced129
Nebin K Raj <96125944+nebink@users.noreply.github.com>
2022-06-18 20:23:22 +0530

    Add files via upload

---
+++  inserting array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the no. of values to inserted in the array :");
+    scanf("%d",&a);
+    int (arr[a]);
+    for(int i; i<a; i++){
+        printf("\nenter the values");
+        scanf("%d",&arr[i]);
+    }
+    
+    return 0;
+}
