1ac957021400040ad6adb33eb0d538fd75cc172f
Nebin K Raj <96125944+nebink@users.noreply.github.com>
2022-07-03 23:14:25 +0530

    Add files via upload

---
+++ Factorial of the number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    printf("For finding the factorial of the no.");
+    int fact=1;
+    printf("/nEnter the no. :");
+    int j;
+    scanf("%d",&j);
+    for(int i=1; i<=j; i++){
+        fact=fact*i;
+    }
+    printf("The factorial of the given number = %d",fact);
+    
+    return 0;
+}
